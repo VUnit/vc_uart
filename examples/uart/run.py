e86dc22000595d46a035b4d8710c8becc379eb18
1138-4EB <1138-4EB@users.noreply.github.com>
2019-06-12 03:35:34 +0200

    revert "use 'ui.use_verification_components'"
    This reverts commit e56129b97e089ea118aa70e29f2f9cab886bc839.

--- examples/uart/run.py
+++ examples/uart/run.py
@@ -12,25 +12,20 @@ A more realistic test bench of an UART to show VUnit VHDL usage on a
 typical module.
 """
 
-from os import environ
-from os.path import join, dirname, abspath
+from os.path import join, dirname
 from vunit import VUnit
-
 
 root = dirname(__file__)
 src_path = join(root, "src")
 
 ui = VUnit.from_argv()
 ui.add_osvvm()
-wh = ui.add_verification_components(abspath(join(root, '..', '..', '..')))
+ui.add_verification_components()
 
-try:
-    if environ['CI']:
-        wh['AXI']['path'] = abspath(join(root, '..', '..', 'vc_axi'))
-except KeyError as err:
-    print('Could not retrieve envvar', err)
-
-ui.use_verification_components(wh, ['UART'])
+ui.library("vunit_lib").add_source_files([
+  join(root, '..', '..', 'vc_axi', 'src', '*.vhd'),
+  join(root, '..', '..', 'src', '*.vhd')
+])
 
 ui.add_library("uart_lib").add_source_files(join(src_path, "*.vhd"))
 ui.add_library("tb_uart_lib").add_source_files(join(src_path, "test", "*.vhd"))
